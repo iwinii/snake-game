5eb2d080733c29793e8e76e39e380e6a67b9c670
Iza Kasznicka <eblondik12@gmail.com>
2022-03-29 22:57:01 +0200

    Added a score, "food", food and snake's collisions and snake's body.

--- snake.py
+++ snake.py
@@ -1,7 +1,11 @@
 import turtle
 import time
+import random
 
 delay = 0.05
+
+# Score
+score = 0
 
 wn = turtle.Screen()
 wn.setup(width=600, height=600)
@@ -16,8 +20,28 @@ head.speed(0)
 head.penup()
 head.color("black")
 head.goto(0, 0)
+#head.shapesize(0.5)
 head.direction = "stop"
 
+# Snake food
+food = turtle.Turtle()
+food.shape("circle")
+food.speed(0)
+food.penup()
+food.color("red")
+#food.shapesize(0.5)
+food.goto(0, 100)
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Score: 0", align="center", font=("Courier", 24, "normal"))
+
+segments = []
 
 # Functions
 def move():
@@ -64,6 +88,38 @@ wn.onkeypress(go_left, "Left")
 # Main loop
 while True:
     wn.update()
+
+    # Eating food
+    if head.distance(food) < 20:
+        x = random.randint(-290, 290)
+        y = random.randint(-290, 290)
+        food.goto(x, y)
+        score += 1
+        pen.clear()
+        pen.write("Score: {}".format(score), align="center", font=("Courier", 24, "normal"))
+
+        # Growing snake's body
+        new_segment = turtle.Turtle()
+        new_segment.shape("square")
+        new_segment.speed(0)
+        new_segment.penup()
+        new_segment.color("grey")
+        new_segment.goto(0, 0)
+        segments.append(new_segment)
+
+    # Move segments in a reverse order
+    for index in range(len(segments)-1, 0, -1):
+        x = segments[index-1].xcor()
+        y = segments[index-1].ycor()
+        segments[index].goto(x, y)
+
+    # Move first segment where the head is
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x, y)
+
+
 
     move()
 
